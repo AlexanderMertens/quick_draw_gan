8f866503427a267350ce7f162297c54d0255fa62
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-11-16 15:18:25 +0100

    Fix small bug in split_data

--- src/data_help/make_dataset.py
+++ src/data_help/make_dataset.py
@@ -10,7 +10,6 @@ def load_dogs(full_size=1000, test_size=0.2, path="./data/raw/dog.npy", verbose=
         print(dogs.dtype)
         print("----------")
 
-    Y = np.full((dogs.shape[0], 2), [1, 0])
     dogs = (dogs.astype(np.float32)) / 255
     dogs = dogs.reshape(dogs.shape[0], 784)
     return dogs
@@ -19,6 +18,7 @@ def load_dogs(full_size=1000, test_size=0.2, path="./data/raw/dog.npy", verbose=
 def split_data(full_size=1000, test_size=0.2, path="./data/raw/dog.npy", verbose=False):
     dogs = load_dogs(full_size=full_size, test_size=test_size,
                      path=path, verbose=verbose)
+    Y = np.full((dogs.shape[0], 2), [1, 0])
     (x_train, y_train, x_test, y_test) = train_test_split(
         dogs, Y, test_size=test_size)
     return (x_train, y_train, x_test, y_test)
