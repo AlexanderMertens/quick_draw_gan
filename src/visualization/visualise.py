15668cc8fd47b51c6baf2dd12f67ad511f499438
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-11-16 15:07:57 +0100

    Create general function to plot images

--- src/visualization/visualise.py
+++ src/visualization/visualise.py
@@ -4,8 +4,13 @@ from data_help.make_dataset import load_dogs
 
 def visualize_training_data(amount_rows=10, show=False, save=False):
     amount = amount_rows * 10
-    images = load_dogs(full_size=amount, verbose=True).reshape(amount, 28, 28)
+    images = load_dogs(full_size=amount).reshape(amount, 28, 28)
+    plot_images(images, path="figures/training_data.png",
+                amount_rows=amount_rows, show=show, save=save)
 
+
+def plot_images(images, path, amount_rows, show, save):
+    amount = amount_rows * 10
     plt.figure(figsize=(amount_rows, 10))
     for i in range(amount):
         plt.subplot(amount_rows, 10, i+1)
@@ -13,7 +18,7 @@ def visualize_training_data(amount_rows=10, show=False, save=False):
         plt.axis('off')
     plt.tight_layout()
     if save:
-        plt.savefig("figures/training_data.png")
+        plt.savefig(path)
 
     if show:
         plt.show()
