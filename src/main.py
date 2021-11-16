f52558fc7e800328ef19aa3814466e5dc009fd49
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-11-16 15:03:29 +0100

    Write visualize_training_data
    Write function that outputs drawings in training data.

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from visualization.visualise import visualize_training_data
+
+
+visualize_training_data(show=True)
--- src/visualization/visualise.py
+++ src/visualization/visualise.py
@@ -1,1 +1,19 @@
-def visualize_training():
+import matplotlib.pyplot as plt
+from data_help.make_dataset import load_dogs
+
+
+def visualize_training_data(amount_rows=10, show=False, save=False):
+    amount = amount_rows * 10
+    images = load_dogs(full_size=amount, verbose=True).reshape(amount, 28, 28)
+
+    plt.figure(figsize=(amount_rows, 10))
+    for i in range(amount):
+        plt.subplot(amount_rows, 10, i+1)
+        plt.imshow(images[i], cmap='gray_r')
+        plt.axis('off')
+    plt.tight_layout()
+    if save:
+        plt.savefig("figures/training_data.png")
+
+    if show:
+        plt.show()
