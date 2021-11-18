fa27b560c6abe535048045122504bb69582e6908
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-11-18 11:25:10 +0100

    rewrite load_data function

--- src/data_help/make_dataset.py
+++ src/data_help/make_dataset.py
@@ -2,23 +2,21 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 
-def load_dogs(full_size=1000, test_size=0.2, path="./data/raw/dog.npy", verbose=False):
-    dogs = np.load(path)[0:full_size]
+def load_data(path, full_size=1000, test_size=0.2, verbose=False):
+    data = np.load(path)[0:full_size]
     if verbose:
-        print("--dogs metadata--")
-        print(dogs.shape)
-        print(dogs.dtype)
+        print("--data metadata--")
+        print(data.shape)
+        print(data.dtype)
         print("----------")
 
-    dogs = (dogs.astype(np.float32)) / 255
-    dogs = dogs.reshape(dogs.shape[0], 784)
-    return dogs
+    # resize data to fit in [0, 1]
+    data = (data.astype(np.float32)) / 255
+    return data
 
 
-def split_data(full_size=1000, test_size=0.2, path="./data/raw/dog.npy", verbose=False):
-    dogs = load_dogs(full_size=full_size, test_size=test_size,
-                     path=path, verbose=verbose)
-    Y = np.full((dogs.shape[0], 2), [1, 0])
+def split_data(data, full_size=1000, test_size=0.2, verbose=False):
+    Y = np.full((data.shape[0], 2), [1, 0])
     (x_train, y_train, x_test, y_test) = train_test_split(
-        dogs, Y, test_size=test_size)
+        data, Y, test_size=test_size)
     return (x_train, y_train, x_test, y_test)
