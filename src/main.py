6b156579e0fd1bd47b4357f86169a386a5d1c23f
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-11-19 13:22:33 +0100

    Write functions for building GAN

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from data_help.make_dataset import CAT_DATA_PATH, DOG_DATA_PATH, create_cats_and_dogs_data, load_data, split_data
-from models.build_model import build_cats_and_dogs_discriminator
+from models.build_model import build_discriminator
 from visualization.visualise import plot_history, plot_images, visualize_training_data
 
 
 size = 5000
 x_train, x_test, y_train, y_test = create_cats_and_dogs_data(size)
 
-model = build_cats_and_dogs_discriminator()
-model.compile('adam', loss='binary_crossentropy', metrics=['binary_accuracy'])
+model = build_discriminator()
 history = model.fit(x=x_train, y=y_train, batch_size=500,
                     epochs=50, validation_data=(x_test, y_test))
 
--- src/models/build_model.py
+++ src/models/build_model.py
@@ -1,7 +1,7 @@
 from tensorflow import keras
 
 
-def build_cats_and_dogs_discriminator(num_filters=8, filter_size=3, pool_size=2):
+def build_discriminator(num_filters=8, filter_size=3, pool_size=2):
     model = keras.models.Sequential([
         keras.layers.Conv2D(num_filters, filter_size, input_shape=(28, 28, 1)),
         keras.layers.MaxPooling2D(pool_size=pool_size),
@@ -9,4 +9,32 @@ def build_cats_and_dogs_discriminator(num_filters=8, filter_size=3, pool_size=2)
         keras.layers.Dense(1, activation='sigmoid')
     ])
 
+    model.compile('adam', loss='binary_crossentropy',
+                  metrics=['binary_accuracy'])
     return model
+
+
+def build_generator():
+    model = keras.models.Sequential([
+        keras.Input(shape=(28, 28, 1)),
+        keras.layers.Dense(128),
+        keras.layers.LeakyReLU(0.3),
+        keras.layers.Dense(256),
+        keras.layers.LeakyReLU(0.3),
+        keras.layers.Dense(784, activation='tanh')
+    ])
+
+    return model
+
+
+def build_GAN():
+    discriminator = build_discriminator()
+    generator = build_generator()
+    discriminator.trainable = False
+    gan = keras.models.Sequential([
+        generator,
+        discriminator
+    ])
+    gan.compile('adam', loss='binary_crossentropy',
+                metrics=['binary_accuracy'])
+    return generator, discriminator, gan
