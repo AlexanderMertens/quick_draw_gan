f23303c15b468753e66253d4bd58ef46ed2c0541
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-11-25 10:02:08 +0100

    Limit number of cores

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+import tensorflow as tf
+
 from mlflow.tracking.fluent import end_run, log_artifact, start_run
 from mlflow.keras import log_model
 
 from models.train_model import train_model
 
+tf.config.threading.set_intra_op_parallelism_threads(2)
+tf.config.threading.set_inter_op_parallelism_threads(2)
 start_run()
 
 gan = train_model(num_epochs=1, num_batch=4, batch_size=8)
