bd936749da2b9061204eb7b98f78b4f92b0e72d5
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-11-29 14:48:27 +0100

    close #5

--- src/main.py
+++ src/main.py
@@ -4,13 +4,11 @@ from mlflow.tracking.fluent import end_run, log_artifact, start_run
 
 from models.train_model import train_model
 
-tf.config.threading.set_intra_op_parallelism_threads(2)
-tf.config.threading.set_inter_op_parallelism_threads(2)
 run_name = 'GAN-v1-envelope'
 start_run(run_name=run_name)
 
-gan = train_model(num_epochs=100, num_batch=64,
-                  batch_size=128, run_name=run_name)
+gan = train_model(num_epochs=100, num_batch=16,
+                  batch_size=512, run_name=run_name)
 log_artifact('./figures/results', artifact_path='figures')
 
 end_run()
