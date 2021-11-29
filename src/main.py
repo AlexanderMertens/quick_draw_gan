c124a1c1062ed2023f417349f4b42346a4383180
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-11-29 11:35:04 +0100

    Adjust model

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ from mlflow.tracking.fluent import end_run, log_artifact, start_run
 
 from models.train_model import train_model
 
-run_name = 'WGAN-v1-filtered-envelope'
+run_name = 'WGAN-v3-filtered-envelope'
 start_run(run_name=run_name)
 
-gan = train_model(num_epochs=10, n_disc=5, num_batch=1,
-                  batch_size=64, run_name=run_name)
+gan = train_model(num_epochs=40, n_disc=5, num_batch=16,
+                  batch_size=256, run_name=run_name)
 log_artifact('./figures/results', artifact_path='figures')
 
 end_run()
--- src/models/build_model.py
+++ src/models/build_model.py
@@ -7,8 +7,9 @@ import data_help.data_constants as dc
 def build_discriminator():
     depth = 64
     dropout = 0.4
-    kernel_size = 5
+    kernel_size = 4
     constraint = ClipConstraint(0.01)
+    init = keras.initializers.RandomNormal(stddev=0.02)
     input_shape = (dc.IMAGE_WIDTH, dc.IMAGE_WIDTH, 1)
 
     log_param('Discriminator depth', depth)
@@ -16,21 +17,15 @@ def build_discriminator():
     log_param('Discriminator kernel size', kernel_size)
     model = keras.models.Sequential([
         # In: (28, 28, 1)
-        keras.layers.Conv2D(depth, kernel_size=kernel_size, strides=2, kernel_constraint=constraint,
+        keras.layers.Conv2D(depth, kernel_size=kernel_size, strides=2, kernel_constraint=constraint, kernel_initializer=init,
                             input_shape=input_shape, padding='same'),
-        keras.layers.Dropout(dropout),
+        keras.layers.BatchNormalization(momentum=0.9),
+        keras.layers.LeakyReLU(alpha=0.2),
 
-        keras.layers.Conv2D(depth*2, kernel_size=kernel_size, kernel_constraint=constraint,
+        keras.layers.Conv2D(depth*2, kernel_size=kernel_size, kernel_constraint=constraint, kernel_initializer=init,
                             strides=2, padding='same'),
-        keras.layers.Dropout(dropout),
-
-        keras.layers.Conv2D(depth*4, kernel_size=kernel_size, kernel_constraint=constraint,
-                            strides=2, padding='same'),
-        keras.layers.Dropout(dropout),
-
-        keras.layers.Conv2D(depth*8, kernel_size=kernel_size, kernel_constraint=constraint,
-                            strides=1, padding='same'),
-        keras.layers.Dropout(dropout),
+        keras.layers.BatchNormalization(momentum=0.9),
+        keras.layers.LeakyReLU(alpha=0.2),
 
         keras.layers.Flatten(),
         keras.layers.Dense(1),
@@ -45,36 +40,32 @@ def build_discriminator():
 
 def build_generator():
     dropout = 0.4
-    depth = 256
+    depth = 128
     dim = 7
+    init = keras.initializers.RandomNormal(stddev=0.02)
 
     log_param('Generator depth', depth)
     log_param('Generator dropout rate', dropout)
 
     model = keras.models.Sequential([
-        keras.layers.Dense(dim*dim*depth, input_dim=dc.LATENT_DIM),
-        keras.layers.BatchNormalization(axis=-1, momentum=0.9),
-        keras.layers.Activation('relu'),
+        keras.layers.Dense(
+            dim*dim*depth, input_dim=dc.LATENT_DIM, kernel_initializer=init),
+        keras.layers.LeakyReLU(alpha=0.2),
         keras.layers.Reshape((dim, dim, depth)),
-        keras.layers.Dropout(dropout),
 
-        keras.layers.UpSampling2D(),
-        keras.layers.Conv2DTranspose(int(depth/2), 5, padding='same'),
+        keras.layers.Conv2DTranspose(
+            depth, kernel_size=4, strides=2, padding='same', kernel_initializer=init),
         keras.layers.BatchNormalization(axis=-1, momentum=0.9),
-        keras.layers.Activation('relu'),
+        keras.layers.LeakyReLU(alpha=0.2),
 
-        keras.layers.UpSampling2D(),
-        keras.layers.Conv2DTranspose(int(depth/4), 5, padding='same'),
+        keras.layers.Conv2DTranspose(
+            depth, kernel_size=4, strides=2, padding='same', kernel_initializer=init),
         keras.layers.BatchNormalization(axis=-1, momentum=0.9),
-        keras.layers.Activation('relu'),
-
-        keras.layers.Conv2DTranspose(int(depth/8), 5, padding='same'),
-        keras.layers.BatchNormalization(axis=-1, momentum=0.9),
-        keras.layers.Activation('relu'),
+        keras.layers.LeakyReLU(alpha=0.2),
 
         # Out: 28 x 28 x 1 grayscale image [-1, 1] per pix
-        keras.layers.Conv2DTranspose(1, 5, padding='same'),
-        keras.layers.Activation('tanh')
+        keras.layers.Conv2D(1, 7, padding='same',
+                            activation='tanh', kernel_initializer=init),
     ])
 
     return model
